d8278b84ee6a439ce79543ab547e9dea951bf55f
vignesh <vigneshpoluru1277@gmail.com>
2025-09-24 11:52:01 +0530

    Update

--- BACKEND/LibraryBookManagementSystem/src/main/java/com/example/demo/LibraryBookManagementSystemApplication.java
+++ BACKEND/LibraryBookManagementSystem/src/main/java/com/example/demo/LibraryBookManagementSystemApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryBookManagementSystemApplication {
+public class LibraryBookManagementSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryBookManagementSystemApplication.class, args);
+				System.out.println("Project is Running ...");
 		
 	}
 
